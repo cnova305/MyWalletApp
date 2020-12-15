05e6b9c647ce93f6f3f3c412d81165b9cba449f9
Christopher Juwao <njuwao@icloud.com>
2020-12-15 16:09:09 +0200

    Initial Commit

---
+++ MyWalletApp/MyWalletAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWalletAppApp.swift
+//  MyWalletApp
+//
+//  Created by Christopher Juwao on 2020/12/15.
+//
+
+import SwiftUI
+
+@main
+struct MyWalletAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
